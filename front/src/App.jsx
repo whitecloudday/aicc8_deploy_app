be47b6dad87a0ce422ca48362b0171f50f66b57a
whitecloudday <cvbin89@gmail.com>
2026-01-30 12:20:23 +0900

    log

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,14 +1,25 @@
 import './App.css'
+import { BrowserRouter as Router, Routes, Route, BrowserRouter } from 'react-router-dom'
+import Home from './components/Home'
+import Completed from './components/Completed'
+import Important from './components/Important'
+import Proceeding from './components/Proceeding'
 
 const App = () => {
 
   return (
-  
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-      </h1>
-   
-  )
-}
+   <BrowserRouter>
+     <div>
+       <Routes>
+         <Route path='/' element={<Home />} />
+         <Route path='/completed' element={<Completed />} />
+         <Route path='/important' element={<Important />} />
+         <Route path='/proceeding' element={<Proceeding />} />
+       </Routes>
+     </div>
+   </BrowserRouter>
+  );
+};
+
 
 export default App
