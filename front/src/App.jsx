ca21e0f585666a0f6dac8a681e3f50ae49c68a0c
whitecloudday <cvbin89@gmail.com>
2026-01-30 18:16:59 +0900

    env test

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,25 +1,28 @@
-import './App.css'
-import { BrowserRouter as Router, Routes, Route, BrowserRouter } from 'react-router-dom'
-import Home from './components/Home'
-import Completed from './components/Completed'
-import Important from './components/Important'
-import Proceeding from './components/Proceeding'
+import './App.css';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  BrowserRouter,
+} from 'react-router-dom';
+import Home from './components/Home';
+import Completed from './components/Completed';
+import Important from './components/Important';
+import Proceeding from './components/Proceeding';
 
 const App = () => {
-
   return (
-   <BrowserRouter>
-     <div>
-       <Routes>
-         <Route path='/' element={<Home />} />
-         <Route path='/completed' element={<Completed />} />
-         <Route path='/important' element={<Important />} />
-         <Route path='/proceeding' element={<Proceeding />} />
-       </Routes>
-     </div>
-   </BrowserRouter>
+    <BrowserRouter>
+      <div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/completed" element={<Completed />} />
+          <Route path="/important" element={<Important />} />
+          <Route path="/proceeding" element={<Proceeding />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 };
 
-
-export default App
+export default App;
